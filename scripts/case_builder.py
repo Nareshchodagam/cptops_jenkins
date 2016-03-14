230d72ac0cce605fcd898c54477c5411010cbd41
Mitchell Gaddy <mgaddy@salesforce.com>
2016-03-13 22:26:15 -0400

    Added Canary logic in case_builder Update case_presets with Canary mq

--- scripts/case_builder.py
+++ scripts/case_builder.py
@@ -4,7 +4,6 @@
 import json
 import subprocess
 from subprocess import PIPE,Popen
-import sets
 import os
 import cmd
 
@@ -13,36 +12,48 @@ def json_imports():
     ver = "valid_versions.json"
     with open(presets, 'r') as p:
         sets = json.load(p)
-    
-    with open(ver, 'r') as v:
-        bundles = json.load(v)
-    
-    return sets,bundles    
-    
-def pod_builder(sets, bundles):
-    role = os.environ['ROLE']
+
+    #with open(ver, 'r') as v:
+        #bundles = json.load(v)
+    return sets
+
+def pod_builder(sets):
+    role = os.environ['ROLE'].lower()
     bundle = os.environ['BUNDLE']
     dr = os.environ['DR']
     canary = os.environ['CANARY']
     monthDict={'01':'Jan', '02':'Feb', '03':'Mar', '04':'Apr', '05':'May', '06':'Jun', '07':'Jul', '08':'Aug', '09':'Sep', '10':'Oct', '11':'Nov', '12':'Dec'}
     mon = monthDict[bundle.split('.')[1]]
-    if dr:
+
+    if os.environ['DR'] == "false":
         status = "Prod"
     else:
         status = "DR"
-    
-    if not canary:
+
+    if os.environ['CANARY'] == "false":
         group_file = sets[role][status]['PODGROUP']
         template = sets[role][status]['TEMPLATEID']
         gsize = sets[role][status]['GROUPSIZE']
         tagsize = sets[role][status]['TAGGROUPS']
+
+        cmd = "python bin/pods_cases.py -p hostlists/%s -r %s -t %s -b %s -d %s -s %d -g \"%s\" --patchset %s --taggroups %d" % (group_file, role, template, mon.lower(), dr.title(), gsize, status, bundle, tagsize)
+        p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        with open("cases.sh", 'w') as f:
+            f.write(p.stdout.read())
+    else:
+        group_file = sets[role]['CANARY'][status]['PODGROUP']
+        template = sets[role]['CANARY'][status]['TEMPLATEID']
+        gsize = sets[role]['CANARY'][status]['GROUPSIZE']
+        tagsize = sets[role]['CANARY'][status]['TAGGROUPS']
         
-        cmd = "python pods_cases.py -p %s -r %s -t %s -b %s -d %s -s %d -g \"%s\" --patchset %s --taggroups %d" (group_file, role, template, mon, dr, gsize, status, bundle, tagsize)
-        print cmd
-        
+        cmd = "python bin/pods_cases.py -p hostlists/%s -r %s -t %s -b %s -d %s -s %d -g \"%s\" --patchset %s --taggroups %d" % (group_file, role, template, mon.lower(), dr.title(), gsize, status, bundle, tagsize)
+        p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        with open("cases.sh", 'w') as f:
+            f.write(p.stdout.read())
+
 if __name__ == "__main__":
-    (sets, bundles) = json_imports()
-    pod_builder(sets, bundles)
+    sets = json_imports()
+    pod_builder(sets)
         
     
     
