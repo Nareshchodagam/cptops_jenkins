daec4b8475c34a3fa836561a02cf147bd7826f44
Mitchell Gaddy <mgaddy@salesforce.com>
2016-05-27 01:37:58 -0400

    Added options file with additional options for gen_cases. updated case_builder script adding dry-run feature

--- scripts/case_builder.py
+++ scripts/case_builder.py
@@ -12,6 +12,9 @@ import re
 import sys
 import logging
 import shlex
+import argparse
+import case_opts as co
+
 
 def json_imports():
     '''
@@ -31,7 +34,7 @@ def hostlist_builder():
     logging.debug('Building updated POD list.....')
     retcode = subprocess.check_call(shlex.split(case_cmd))
     if retcode != 0:
-        logging.error('POD list creation failed')
+        logging.debug('POD list creation failed')
         sys.exit(1)
 
 def cmd_builder(sets):
@@ -45,7 +48,7 @@ def cmd_builder(sets):
     pod_cmd = ["python", "/home/jenkins/git/cptops_case_gen/bin/gen_cases.py" ]
     bld_cmd = {}
     bld_cmd['status'] = "True" if role_status == "DR" else "FALSE"
-    bld_cmd['bundle'] = os.environ['BUNDLE']
+    bld_cmd['bundle'] = os.environ['BUNDLE'] 
     bld_cmd['patchset'] = os.environ['BUNDLE']
     bld_cmd['podgroup'] = "/home/jenkins/hostlist/PODGROUP" if os.environ['PODGROUP'] != "" else "/home/jenkins/git/cptops_case_gen/hostlists/" + sets[role_class][role_status]['PODGROUP']
     bld_cmd['gsize'] = os.environ['GROUPSIZE'] if os.environ['GROUPSIZE'] != "DEFAULT" else sets[role_class][role_status]['GROUPSIZE']
@@ -56,6 +59,16 @@ def cmd_builder(sets):
     bld_cmd['regexfilter'] = os.environ['REGEX']
     bld_cmd['filter'] = os.environ['FILTER']
     bld_cmd['subject'] = os.environ['SUBJECT']
+    
+    #Custom parameters within the case_presets.json
+    #These options are not within each predefined role_class. 
+    #So we test if values are present. 
+    for key in ['CASETYPE', 'IMPL_PLAN']:
+        try:
+            bld_cmd[str.lower(key)] = sets[role_class][role_status][key]
+        except KeyError:
+            if key == "IMPL_PLAN":
+                bld_cmd[str.lower(key)] = os.environ['BUNDLE']
 
     logging.debug("TEMPLATEID = " + bld_cmd['template'])
     logging.debug("GROUPSIZE = " + str(bld_cmd['gsize']))
@@ -65,35 +78,28 @@ def cmd_builder(sets):
     logging.debug("SUBJECT = " + bld_cmd['subject'])
     logging.debug("PODGROUP = " + bld_cmd['podgroup'])
     logging.debug("REGEXFILTER = " + bld_cmd['regexfilter'])
+    logging.debug("BUNDLE = " + bld_cmd['bundle'])
     logging.debug("Contents of uploaded file %s" %  os.environ['PODGROUP'])
     with open(bld_cmd['podgroup'], 'r') as fin:
         print fin.read()
-    pod_builder(bld_cmd)
+    case_builder(bld_cmd)
 
-def pod_builder(bld_cmd):
+def case_builder(bld_cmd):
     '''
     Function that extracts information from case_presets.json. Builds the
     pod_cases.py command string.
     '''
-    case_file = '/home/jenkins/git/cases.sh'
-    opt_dict = {'gsize': '-s',
-                'role': '-r',
-                'podgroup': '-p',
-                'template': '-t',
-                'status': '-d',
-                'bundle': '-b',
-                'patchset': '--patchset',
-                'tagsize': '--taggroups',
-                'infra': '--infra',
-                'gsize': '-s',
-                'filter': '-f',
-                'regexfilter': '--regexfilter' }
+    cmd = ''
+    case_file = '/home/jenkins/output/cases.sh'
     pod_cmd = ["python", "/home/jenkins/git/cptops_case_gen/bin/gen_cases.py" ]
-    for opt in opt_dict.iterkeys():
+    for opt in co.opt_dict.iterkeys():
         if bld_cmd.has_key(opt) and bld_cmd[opt] != "":
-            pod_cmd.append(opt_dict[opt])
+            pod_cmd.append(co.opt_dict[opt])
             pod_cmd.append(str(bld_cmd[opt]))
-    logging.debug(pod_cmd)
+    for item in pod_cmd:
+        cmd += str(item)
+        cmd += " "
+    logging.debug(cmd)
     file_proc = subprocess.Popen(pod_cmd, stdout=subprocess.PIPE)
     with open(case_file, 'w') as cases:
         cases.write(file_proc.stdout.read())
@@ -103,7 +109,7 @@ def case_executor():
     gus cases. Gus_cases.py requires proxy setting while build_plan.py
     does not require proxy setting. Hence the loop.
     '''
-    case_file = '/home/jenkins/git/cases.sh'
+    case_file = '/home/jenkins/output/cases.sh'
     cmd_type = re.compile(r'^(python\s[a-z_.]*)')
     pods = re.compile(r'--inst\s([A-Za-z0-99,]*)')
     os.chdir('/home/jenkins/git/cptops_case_gen/bin')
@@ -135,13 +141,19 @@ def case_executor():
     return failed_plans
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Case Builder Program    ')
+    parser.add_argument("--dry-run", action="store_true", dest="dryrun",
+                        help="Dry run of build no case will be generated.")
+    options = parser.parse_args()
+    
     logging.basicConfig(level=logging.DEBUG)
     sets = json_imports()
     if os.environ['PODGROUP'] == "":
         hostlist_builder()
     cmd_builder(sets)
-    failures = case_executor()
-    if failures:
-        for fail in failures:
-            logging.debug(str(fail) + " failed to generate implementation plans.")
-        sys.exit(1)
+    if not options.dryrun:
+        failures = case_executor()
+        if failures:
+            for fail in failures:
+                logging.debug(str(fail) + " failed to generate implementation plans.")
+            sys.exit(1)
